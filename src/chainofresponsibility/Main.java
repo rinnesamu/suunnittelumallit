6c3472a068cdadc1596ac72e7d0dad0542475eaa
Samu <samu.rinne@metropolia.fi>
2020-04-14 14:39:36 +0300

    type bosses as handler

--- src/chainofresponsibility/Main.java
+++ src/chainofresponsibility/Main.java
@@ -3,17 +3,15 @@ package chainofresponsibility;
 public class Main {
 
 	public static void main(String[] args) {
-		CEO c = new CEO();
-		Boss b = new Boss();
-		Manager m = new Manager();
+		Handler c = new CEO();
+		Handler b = new Boss();
+		Handler m = new Manager();
 		m.setHandler(b);
 		b.setHandler(c);
 		m.askRaise(2);
 		m.askRaise(5);
 		m.askRaise(10);
 		m.askRaise(11);
-		
-
 	}
 
 }
